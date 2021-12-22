2e1a84d109ae70e367be9f9d2e22631640e3801e
Cam Remesz <lowkey@Cams-Air.cg.shawcable.net>
2021-12-21 22:57:12 -0700

    Added overlays store

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { UsersContextProvider } from "./store/users-context";
+import { OverlaysContextProvider } from "./store/overlays-context";
+
 import Accounts from "./components/pages/accounts/Accounts/Accounts";
 import Overview from "./components/pages/overview/Overview/Overview";
 import AddPerson from "./components/pages/addPerson/AddPerson/AddPerson";
 import Settings from "./components/pages/settings/Settings/Settings";
 
+
 function App() {
 
   return (
-    <UsersContextProvider>
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<Overview />} />
+    <OverlaysContextProvider>
+      <UsersContextProvider>
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<Overview />} />
 
-          <Route exact path="/accounts" element={<Accounts />} />
+            <Route exact path="/accounts" element={<Accounts />} />
 
-          <Route exact path="/addPerson" element={<AddPerson />} />
+            <Route exact path="/addPerson" element={<AddPerson />} />
 
-          <Route exact path="/settings" element={<Settings />} />
-        </Routes>
-      </Router>
-    </UsersContextProvider>
+            <Route exact path="/settings" element={<Settings />} />
+          </Routes>
+        </Router>
+      </UsersContextProvider>
+    </OverlaysContextProvider>
   );
 }
 
--- src/components/UI/Overlay/Overlay.js
+++ src/components/UI/Overlay/Overlay.js
@@ -1,15 +1,15 @@
 import { useContext } from 'react';
 
-import UsersContext from '../../../store/users-context';
+import OverlaysContext from '../../../store/overlays-context';
 
 import styles from './Overlay.module.css';
 
 const Overlay = () => {
 
-const usersCtx = useContext(UsersContext);
+const overlaysCtx = useContext(OverlaysContext);
 
   return (
-    <div className={styles.overlay} onClick={()=> usersCtx.setTransferModalOpen(false)}/>
+    <div className={styles.overlay} onClick={()=> overlaysCtx.setTransferModalOpen(false)}/>
   )
 }
 
